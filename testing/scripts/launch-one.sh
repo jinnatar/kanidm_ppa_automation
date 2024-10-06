5ed0b6eeb631e5920d0e09f9c12ae362b84d3de3
Jinna Kiisuo <jinnak@nocturnal.fi>
2024-10-06 19:41:01 +0300

    WIP: make aarch64 qemu boot

--- testing/scripts/launch-one.sh
+++ testing/scripts/launch-one.sh
@@ -8,26 +8,31 @@ debglob="${2?}"
 
 SSH_PORT="${SSH_PORT:-222}"
 
+>&2 echo "Generating seed.img"
 cloud-localds seed.img user-data.yaml
+
+>&2 echo "Generating EFI artifacts"
 
 case "$arch" in
 	x86_64)
 		MACHINE=q35,accel=kvm
 		EFI=/usr/share/OVMF/OVMF_CODE.fd
+		VARSTORE=""
+		DRIVE="-drive if=virtio,format=qcow2,file=${img}"
 		;;
 	aarch64)
 		MACHINE=virt,gic-version=3
 		
-		# Make a bespoke EFI img
-		#truncate -s 64m "${arch}_varstore.img"
+		truncate -s 64m "${arch}_varstore.img"
 		truncate -s 64m "${arch}_efi.img"
 		dd if=/usr/share/qemu-efi-aarch64/QEMU_EFI.fd of="${arch}_efi.img" conv=notrunc
+		VARSTORE="-drive if=pflash,format=raw,file=${arch}_varstore.img"
 		EFI="${arch}_efi.img"
-		#VARSTORE="${arch}_varstore.img"
+		DRIVE="-drive if=none,file=${img},id=hd0 -device virtio-blk-device,drive=hd0"
 		;;
 	*)
-		machine=virt
-		EFI=efi.img # :shrug: .. you figure it out.
+		>&2 echo "Unsupported architecture"
+		exit 1
 		;;
 esac
 
@@ -35,12 +40,12 @@ esac
 
 "qemu-system-$arch"  \
   -machine type="${MACHINE}" -m 1024 \
-  -boot d \
+  -cpu max \
   -nographic \
   -snapshot \
-  -netdev id=net00,type=user,hostfwd=tcp::"${SSH_PORT}"-:22 \
-  -device virtio-net-pci,netdev=net00 \
   -drive if=pflash,format=raw,file=${EFI},readonly=on \
-  -drive if=virtio,format=qcow2,file="${img}" \
-  -drive if=virtio,unit=2,format=raw,file=seed.img
-  #-drive if=pflash,format=raw,file=${VARSTORE} \
+  ${VARSTORE} \
+  ${DRIVE} \
+  -drive if=virtio,format=raw,file=seed.img \
+  -netdev id=net00,type=user,hostfwd=tcp::"${SSH_PORT}"-:22 \
+  -device virtio-net-pci,netdev=net00
